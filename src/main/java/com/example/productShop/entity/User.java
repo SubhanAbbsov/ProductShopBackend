d6358651e94b73f6f89597ab9cce7a4ceb69ebb0
Subhan Abbasov <subhanharry777@gmail.com>
2025-04-18 13:08:43 +0300

    add models

---
+++ src/main/java/com/example/productShop/entity/User.java
@@ -0,0 +1,46 @@
+package com.example.productShop.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    @Data
+    @Entity
+    @Table(name = "users")
+    public class user {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @NotBlank(message = "Name is required")
+        @Size(min = 1, max = 50)
+        @Column(unique = true)
+        private String name;
+
+        @NotBlank(message = "Surname is required")
+        @Size(min = 1, max = 50)
+        @Column(unique = true)
+        private String surname;
+
+        @Email(message = "Invalid email format")
+        @NotBlank(message = "Email is required")
+        @Column(unique = true)
+        private String email;
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 1)
+        private String password;
+        @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+        private List<Product> products = new ArrayList<>();
+
+        @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+        private List<CartItem> products = new ArrayList<>();
+    }
+}
